9ba82654ab21da354dc19d27e3a2062ff7ef9bfd
Gabriel Moraes <moraesgabriel0003@gmail.com>
2024-06-05 23:53:18 -0300

    alteração props

--- projeto-trainee/src/App.jsx
+++ projeto-trainee/src/App.jsx
@@ -9,22 +9,21 @@ import fotoFeed2 from './assets/foto-feed-2.jpg'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState(
-    {
+  const user = {
       nome: "Caio Henrique Noronha",
       idade: "19",
       bio: "Exemplo de bio; Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta debitis incidunt assumenda tempora ut explicabo error, est libero fugit.",
       profilePicture: fotoPerfil,
       feedPictures: [fotoPerfil,fotoFeed1,fotoFeed2]
     }
-  )
+
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Navigate to='/login' />} />
         <Route path='/login' element={<Login />}/>
         <Route path='/registro' element={<Registro />}/>
-        <Route path='/user-page' element={<UserPage user={user} setUser={setUser} />}/>
+        <Route path='/user-page' element={<UserPage user={user}/>}/>
       </Routes>
     </Router>
   )
